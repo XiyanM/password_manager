584759a954f6e2a32339b24b9ea79c83de186403
Xiyan Moo <xiy4nm@gmail.com>
2025-08-24 10:17:49 +0800

    finished

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import json
 from cryptography.fernet import Fernet
 import os
+import getpass
 
 
 def get_vault(path = "pass.json"):
       if not os.path.exists(path):
             return {}
-      else:
+      try:
             with open(path, "r") as f:
                   return json.load(f)
+      except json.JSONDecodeError:
+           return {}
+           
         
       
       
@@ -16,97 +20,92 @@ def get_vault(path = "pass.json"):
 def get_fernet(path = "key.key"):
      if not os.path.exists(path):
            key = Fernet.generate_key()
-           with open("key.key", "wb") as f:
+           with open(path, "wb") as f:
                 f.write(key)
 
      else:
-           with open("key.key", "rb") as f:
+           with open(path, "rb") as f:
                  key = f.read()
      return Fernet(key)
           
 
-def encrypt(vault, fernet):
-      for platform in vault:
-          ptpassword = vault[platform]["password"]
-          if not ptpassword.startswith("gAAAA"):
-             token = fernet.encrypt(ptpassword.encode("utf-8"))
-             vault[platform]["password"] = token.decode("utf-8")
-      with open("pass.json", "w") as f:
-         json.dump(vault, f, indent=4)
-
 def add(service, username, password, fernet, path = "pass.json"):
      vault = get_vault()
-     vault[service] = {"username": username, "password": password}
+     vault[service] = {"username": username, "password": (fernet.encrypt(password.encode("utf-8"))).decode()}
      with open(path, "w") as f:
           json.dump(vault, f, indent=4)
-     encrypt(vault, fernet)
           
           
 
 def main():
      fernet = get_fernet()
-     userinput = input("Actions: add get list update delete\n")
+     while True:
+        userinput = input("Actions: add get list update delete quit\n")
 
-     if userinput == "add":
-          line = input("Format: service username password").strip()
-          items = line.split()
-          if len(items) != 3:
-               return print("Please input exactly 3 fields.")
-          service, username, password = items
-          add(service, username, password, fernet)
-          return print("Added successfully.")
+        if userinput == "add":
+            service = input("Service: ").strip()
+            username = input("Username: ").strip()
+            password = getpass.getpass("Password (input hidden): ")
+            add(service, username, password, fernet)
+            print("Added successfully.")
+            continue
 
-     if userinput == "get":
-          line = input("Input platform and username.").strip()
-          items = line.split()
-          if len(items) != 2:
-               return print("Please input exactly 2 fields.")
-          platform, username = items
-          vault = get_vault()
-          for service, info in vault.items():
-               if service == platform:
-                    if info["username"] == username:
-                         return print(fernet.decrypt(info["password"]).decode("utf-8"))
-                    
-     if userinput == "list":
-          vault = get_vault()
-          print("Platforms:")
-          for service in vault:
-               print( service)
+        elif userinput == "get":
+            service = input("Service: ").strip()
+            username = input("Username: ").strip()
+            vault = get_vault()
+            if service not in vault:
+                print("No such service found.")
+                continue
+            if vault[service]["username"] != username:
+                print("Username mismatch.")
+                continue
+            print((fernet.decrypt(vault[service]["password"].encode("utf-8"))).decode("utf-8"))
+            continue
 
-     if userinput == "update":
-          line = input("Format: service username password\n").strip()
-          items = line.split()
-          service, username, password = items
-          if len(items) != 3:
-               return print("Please input exactly 3 fields.")
-          vault = get_vault()
-          if service not in vault:
-               print("No such service found.")
-          vault[service]["username"] = username
-          vault[service]["password"] = (fernet.encrypt(password.encode("utf-8"))).decode("utf-8")
+    
+        elif userinput == "list":
+            vault = get_vault()
+            print("Platforms:")
+            for service in vault:
+                print(service)
+            continue
 
-          with open("pass.json", "w") as f:
-               json.dump(vault, f, indent=4)
-          print("Information updated successfully.")
+        elif userinput == "update":
+            service = input("Service: ").strip()
+            username = input("Username: ").strip()
+            password = getpass.getpass("Password (input hidden): ")
+            vault = get_vault()
+            if service not in vault:
+                print("No such service found.")
+                continue
+            vault[service]["username"] = username
+            vault[service]["password"] = (fernet.encrypt(password.encode("utf-8"))).decode("utf-8")
 
-     if userinput == "delete":
-          line = input("Input which service to delete.\n").strip()
-          vault = get_vault()
-          if line not in vault:
-               return print("No such service found.")
-          del vault[line]
-          with open("pass.json", "w") as f:
-               json.dump(vault, f, indent=4)
-          return print("Successfully deleted.")
+            with open("pass.json", "w") as f:
+                json.dump(vault, f, indent=4)
+            print("Information updated successfully.")
+            continue
 
-
-
+        elif userinput == "delete":
+            line = input("Input which service to delete.\n").strip()
+            vault = get_vault()
+            if line not in vault:
+                print("No such service found.")
+                continue
+            check = input(f"Delete {line}? (y/N) ").lower()
+            if check == "y":
+                del vault[line]
+                with open("pass.json", "w") as f:
+                    json.dump(vault, f, indent=4)
+                print("Successfully deleted.")
+                continue
+            continue
         
-          
-          
+        elif userinput == "quit":
+            break
 
-          
+
 if __name__ == "__main__":
      main()     
 
